049371cf1e1f4dccaec597eb856e8a6074f808c2
Iamneo <techadmin@iamneo.ai>
2023-08-24 13:00:48 +0000

    Backup Commit

---
+++ demoproject1/src/main/java/com/example/demoproject1/Demoproject1Application.java
@@ -0,0 +1,13 @@
+package com.example.demoproject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demoproject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demoproject1Application.class, args);
+	}
+
+}
